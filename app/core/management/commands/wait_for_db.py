daa336b22d586da550d95b061eb3058b2a4a927c
ADARSH ATLURI <adarshatluri@ADARSHs-MacBook-Pro.local>
2024-05-20 18:27:47 +0530

    Fixed Linting errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,16 +14,18 @@ class Command(BaseCommand):
 # When we call the wait_for_db command , it will call this handle method.
     def handle(self, *args, **options):
         """Entrypoint for command."""
-        # Log a message 
+        # Log a message
         self.stdout.write('Waiting for database...')
         # Set a boolean that tracks if the db is up to false.
         db_up = False
         while db_up is False:
             try:
-                # we call the check method by passing the database as a parameter. 
+                # we call the check method by passing
+                # the database as a parameter.
                 self.check(databases=['default'])
                 db_up = True
-            # If the database is not ready it will raise an exception which is caught in the excep clause.
+            # If the database is not ready it will raise an exception
+            # which is caught in the excep clause.
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
